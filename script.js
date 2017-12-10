a5413cd90e9d88312d6c367a3943fd36dbe9c482
cjbenedikt <cjbenedikt@gmail.com>
2017-12-10 15:00:52 -0500

    javascript

---
+++ script.js
@@ -0,0 +1,21 @@
+var text='Aleatoric+dice';
+console.log(text);
+alert('HEEELP!')
+document.write(text)
+var x=8;
+var y=7;
+var z=x+y;
+console.log(z);
+function randomFact(){console.log('sleep in your eye is called rheum')}
+randomFact();
+function callDog (Balu){console.log('come here,'+ Balu +'!')}
+callDog ('balu');
+function sum(a, b){console.log(a+b)};
+  sum(5, 13);
+  function square(x){return(x*x)}
+var squaredNumber = square(13);
+console.log(squaredNumber);
+var hasDogs = confirm ('do you have dogs');
+
+if (hasDogs){alert('you are awesome');}
+else {alert('you are useless');}
